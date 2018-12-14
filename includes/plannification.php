4b1b33a2d1329aa922a65f464eba2c5e777b3d51
jbrochier <34643041+Bolrod@users.noreply.github.com>
2018-12-14 14:53:24 +0100

    plannification

--- includes/plannification.php
+++ includes/plannification.php
@@ -20,15 +20,58 @@
     </head>
 
 
-    <body>
-        <div class ="container zone" id="plannification">
-            <div class="row">
-                <div class="col-xs-6">
-                    <p class="titre_zone">Réveil / Coucher</p>
+    <body>  
+    <div class ="container">
+        <div class="row espace">
+            <div class = "logoTitre">
+                    <img src = "image/logo.png" title ="Logo" alt = "Logo">
+            </div>
+        </div>
+    </div>
 
+        <div class ="container styled" id="plannification">
+            <div class="row">
+                <div class="col-xs-5">
+                    <p class="titre_zone">Réveil / Coucher</p>
+                    
+                    <form action="plannification.php" method="get">
+                    Je me réveille à : <input class="black" type="time" name="reveil"> </br>
+                    Je veux m'endormir à : <input class="black" type="time" name="couche"></br>
+                    Commencer : <input class="black" type="time" name="couche"> avant </br>
+
+                    Ambiances
+                    <!-- For each ambiance display bouton ambiance -->
+                    <select name='ambiance' size = "2" class="black" required>
+                        <!-- php -->
+                        <option value='ambiance_reveil'> Réveil</option>
+                        <option value='ambiance_couche'> Couché</option>
+                        <option value='ambiance_doux'> Doux</option>
+                    </select>
+
+                    </br> <input class="black" type="submit">
+                    </form>
+                
                 </div>
-                <div class="col-xs-6">
+                <div class="col-xs-2 white_background">
+                </div>
+                <div class="col-xs-5">
                     <p class="titre_zone">Plannification</p>
+
+                    <form action="plannification.php" method="get">
+                    Début : <input class="black" type="time" name="reveil"> 
+                    Fin : <input class="black" type="time" name="couche"></br>
+                   
+                    Ambiances
+                    <!-- For each ambiance display bouton ambiance -->
+                    <select name='ambiance' size = "2" class="black" required>
+                        <!-- php -->
+                        <option value='ambiance_reveil'> Réveil</option>
+                        <option value='ambiance_couche'> Couché</option>
+                        <option value='ambiance_doux'> Doux</option>
+                    </select>
+
+                    </br> <input class="black" type="submit">
+                    </form>
 
                 </div>
             </div>
